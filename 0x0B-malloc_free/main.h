376aa17f409478a63696e9da54caa1ad128330a7
Davicks1 <davidovictor1@gmail.com>
2023-09-05 16:04:13 -0700

    float like butterfly

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+#include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
